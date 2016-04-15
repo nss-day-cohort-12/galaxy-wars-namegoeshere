35cc9688525f9cd4509307d8279277b4846408a7
Matt Qualls <mattqualls@comcast.net>
2016-04-15 17:09:48 -0500

    Preliminary class creation for all three species/interfaces and spacecraft. Species contains properties as well as population declared in program.cs

--- GalaxyWars/GalaxyWars/Program.cs
+++ GalaxyWars/GalaxyWars/Program.cs
@@ -10,6 +10,16 @@ namespace GalaxyWars
     {
         static void Main(string[] args)
         {
+            Daleks daleks = new Daleks();
+            daleks.population = 1000000;
+
+            Sleestak sleestak = new Sleestak();
+            sleestak.population = 1000000;
+
+            Predador predador = new Predador();
+            predador.population = 1000000;
+
+            //Battle Logic
         }
     }
 }
---
+++ GalaxyWars/GalaxyWars/Species.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GalaxyWars
+{
+    class Species
+    {
+        public double population { get; set; }
+        public string health { get; set; }
+        public string language { get; set; }
+        public bool gender { get; set; }
+        public string weapon { get; set;}
+        //can add more once we get into project a little 
+       
+    }
+}
